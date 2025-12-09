20851ea96a09e7dfa63332ac237126a54d43e3c6
cibele-souza <cibele.souza@gmail.com>
2025-12-09 11:23:40 +0100

    implemented tailwind and firebase libraries

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -1,18 +1,16 @@
-import './category-item.styles.scss'
-
 const CategoryItem = ({ category }) => {
    const { imageUrl, title } = category;
    return (
-      <div className='category-container' >
+      <div className='min-w-[30%] h-60 w-80 flex flex-auto flex-col items-center justify-center border border-black mx-2 my-0 mb-4 overflow-hidden group hover:cursor-pointer' >
          <div
-            className='background-image'
+            className='w-full h-full bg-cover bg-center group-hover:scale-110 transition-transform duration-6000 ease-[cubic-bezier(0.25, 0.45, 0.45, 0.95)]'
             style={{
                backgroundImage: `url(${imageUrl})`,
             }}
          />
-         <div className='category-body-container'>
-            <h2>{title}</h2>
-            <p>Shop Now</p>
+         <div className='h-24 px-6 py-0 flex flex-col items-center justify-center border border-black bg-white opacity-70 absolute group-hover:opacity-90'>
+            <h2 className="font-bold my-0 mx-1.5 text-2xl text-gray-700">{title}</h2>
+            <p className="font-light text-base">Shop Now</p>
          </div>
       </div>
 
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,14 +1,11 @@
 import CategoryItem from "../category-item/category-item.component"
-import './directory.styles.scss'
 
 const Directory = ({categories}) => {
    return (
-      <div className="directory-container">
-         <div>
-            {categories.map((category) => (
-               <CategoryItem key={category.id} category={category} />
-            ))}
-         </div>
+      <div className="w-full flex flex-wrap justify-center items-center px-1.5">
+         {categories.map((category) => (
+            <CategoryItem key={category.id} category={category} />
+         ))}
       </div>
    )
 }
