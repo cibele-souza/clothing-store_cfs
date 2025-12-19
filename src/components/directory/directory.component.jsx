b86cf4229a57db27e5cd0266c7cfc020eae34c01
cibele-souza <cibele.souza@gmail.com>
2025-12-19 09:03:45 +0100

    styling fixed

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,13 +1,13 @@
-import CategoryItem from "../category-item/category-item.component"
+import DirectoryItem from '../directory-item/directory-item.component';
 
-const Directory = ({categories}) => {
+const Directory = ({ categories }) => {
    return (
-      <div className="w-full flex flex-wrap justify-center items-center px-1.5">
+      <div className='w-full flex flex-wrap justify-center items-center px-1.5'>
          {categories.map((category) => (
-            <CategoryItem key={category.id} category={category} />
+            <DirectoryItem key={category.id} category={category} />
          ))}
       </div>
-   )
-}
+   );
+};
 
-export default Directory
+export default Directory;
